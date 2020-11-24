22820ea4b69fcb41c9e334e92ce72a963a3e0fbb
russimac <69682485+russimac@users.noreply.github.com>
2020-11-24 00:22:28 -0500

    update

--- public/admin/src/js/script.js
+++ public/admin/src/js/script.js
@@ -7,35 +7,35 @@ function validateForm() {
     var usr_password = document.forms["user_form"]["usr_password"];
     var validate0, validate1, validate2, validate3, validate4, validate5 = false;
     if (usr_name.value == "" || /\d/.test(usr_name.value)) {
-        usr_name.style.boxShadow = "0px 0px 10px red";
+        usr_name.style.boxShadow = "0px 0px 10px #209D9D";
         validate0 = false;
     } else {
         usr_name.style.boxShadow = "";
         validate0 = true;
     }
     if (usr_lname.value == "" || /\d/.test(usr_lname.value)) {
-        usr_lname.style.boxShadow = "0px 0px 10px red";
+        usr_lname.style.boxShadow = "0px 0px 10px #209D9D";
         validate1 = false;
     } else {
         usr_lname.style.boxShadow = "";
         validate1 = true;
     }
     if (usr_id.value == "") {
-        usr_id.style.boxShadow = "0px 0px 10px red";
+        usr_id.style.boxShadow = "0px 0px 10px #209D9D";
         validate2 = false;
     } else {
         usr_id.style.boxShadow = "";
         validate2 = true;
     }
     if (usr_role.value == "") {
-        usr_role.style.boxShadow = "0px 0px 10px red";
+        usr_role.style.boxShadow = "0px 0px 10px #209D9D";
         validate3 = false;
     } else {
         usr_role.style.boxShadow = "";
         validate3 = true;
     }
     if (usr_username.value == "" || usr_username.value.length < 6) {
-        usr_username.style.boxShadow = "0px 0px 10px red";
+        usr_username.style.boxShadow = "0px 0px 10px #209D9D";
         $(document.getElementById("displayError3")).fadeIn("fast");
         validate4 = false;
     } else {
@@ -44,7 +44,7 @@ function validateForm() {
         validate4 = true;
     }
     if (usr_password.value == "" || usr_password.value.length < 8) {
-        usr_password.style.boxShadow = "0px 0px 10px red";
+        usr_password.style.boxShadow = "0px 0px 10px #209D9D";
         $(document.getElementById("displayError4")).fadeIn("fast");
         validate5 = false;
     } else {
@@ -54,6 +54,7 @@ function validateForm() {
     }
     if (validate0 && validate1 && validate2 && validate3 && validate4 && validate5) {
         document.forms["user_form"].submit();
+        alert('Usuario registrado')
     }
 
 }
